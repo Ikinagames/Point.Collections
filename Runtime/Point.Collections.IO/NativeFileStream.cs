5dadd7b0e563377d7286878d16f7372cf878cd6e
Syadeu <syadeu@gmail.com>
2021-12-14 15:31:30 +0900

    no message

---
+++ Runtime/Point.Collections.IO/NativeFileStream.cs
@@ -0,0 +1,112 @@
+﻿// Copyright 2021 Ikina Games
+// Author : Seung Ha Kim (Syadeu)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.IO.LowLevel.Unsafe;
+
+namespace Point.Collections.IO
+{
+    public struct NativeFileStream : IValidation, IDisposable
+    {
+        [NativeDisableUnsafePtrRestriction] private unsafe ReadCommand* m_Command;
+        private ReadHandle m_Handle;
+
+        [BurstDiscard]
+        public byte[] Read(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+        public TReader ReadAsync<TReader>(string path)
+            where TReader : unmanaged, INativeReader
+        {
+#if DEBUG_MODE
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"no files at {path}");
+            }
+#endif
+            unsafe
+            {
+                m_Command = (ReadCommand*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<ReadCommand>(), UnsafeUtility.AlignOf<ReadCommand>(), Allocator.Persistent);
+
+                m_Command->Offset = 0;
+                m_Command->Size = new FileInfo(path).Length;
+                m_Command->Buffer = (byte*)UnsafeUtility.Malloc(m_Command->Size, 16, Allocator.Persistent);
+
+                m_Handle = AsyncReadManager.Read(path, m_Command, 1);
+
+                TReader rdr = new TReader();
+                rdr.Initialize(m_Handle, *m_Command);
+
+                return rdr;
+            }
+        }
+        [BurstDiscard]
+        public void Close()
+        {
+#if DEBUG_MODE
+            if (!IsValid())
+            {
+                UnityEngine.Debug.LogError("Collection is not valid.");
+                return;
+            }
+#endif
+            unsafe
+            {
+                if (m_Command->Buffer != null)
+                {
+                    m_Handle.Dispose();
+                    UnsafeUtility.Free(m_Command->Buffer, Allocator.Persistent);
+                }
+
+                UnsafeUtility.Free(m_Command, Allocator.Persistent);
+            }
+        }
+        void IDisposable.Dispose()
+        {
+#if DEBUG_MODE
+            if (!IsValid())
+            {
+                UnityEngine.Debug.LogError("Collection is not valid.");
+                return;
+            }
+#endif
+            unsafe
+            {
+                if (m_Command->Buffer != null)
+                {
+                    m_Handle.Dispose();
+                    UnsafeUtility.Free(m_Command->Buffer, Allocator.Persistent);
+                }
+
+                UnsafeUtility.Free(m_Command, Allocator.Persistent);
+            }
+        }
+
+        public bool IsValid()
+        {
+            unsafe
+            {
+                if (m_Command == null) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Runtime/Point.Collections.IO/Point.Collections.IO.LowLevel/UnsafeFileReader.cs
@@ -0,0 +1,78 @@
+﻿// Copyright 2021 Ikina Games
+// Author : Seung Ha Kim (Syadeu)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.IO.LowLevel.Unsafe;
+
+namespace Point.Collections.IO.LowLevel
+{
+    public struct UnsafeFileReader : INativeReader, IValidation
+    {
+        private ReadHandle m_Handle;
+        [NativeDisableUnsafePtrRestriction] private unsafe byte* m_Data;
+        private long m_Size;
+
+        public bool IsReadable
+        {
+            get
+            {
+                return m_Handle.Status != ReadStatus.InProgress;
+            }
+        }
+
+        void INativeReader.Initialize(ReadHandle handle, ReadCommand cmd)
+        {
+            m_Handle = handle;
+            unsafe
+            {
+                m_Data = (byte*)cmd.Buffer;
+            }
+            m_Size = cmd.Size;
+        }
+
+        public bool IsValid()
+        {
+            unsafe
+            {
+                if (m_Data == null) return false;
+            }
+
+            return true;
+        }
+
+        public unsafe byte* GetData() => m_Data;
+
+        public T ReadData<T>()
+        {
+            object data;
+            unsafe
+            {
+                if (TypeHelper.TypeOf<T>.Type.Equals(TypeHelper.TypeOf<string>.Type))
+                {
+                    data = Encoding.Default.GetString(m_Data, (int)m_Size);
+                }
+                else
+                {
+                    data = Marshal.PtrToStructure<T>((IntPtr)m_Data);
+                }
+            }
+
+            return (T)data;
+        }
+    }
+}
