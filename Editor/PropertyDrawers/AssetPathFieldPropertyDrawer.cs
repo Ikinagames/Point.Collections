14b9aac421398ce401f98d7d6a8494e5ba03b985
Syadeu <syadeu@gmail.com>
2022-07-11 18:02:33 +0900

    a

--- Editor/PropertyDrawers/AssetPathFieldPropertyDrawer.cs
+++ Editor/PropertyDrawers/AssetPathFieldPropertyDrawer.cs
@@ -49,13 +49,67 @@ namespace Point.Collections.Editor
     }
     internal abstract class AssetPathFieldPropertyDrawerFallback : PropertyDrawerUXML<AssetPathField>
     {
-
-
         private static UnityEngine.Object GetObjectAtPath(in string path)
         {
             if (string.IsNullOrEmpty(path)) return null;
 
             return AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(path);
+        }
+        private static UnityEngine.Object LoadAsset(SerializedProperty property)
+        {
+            string path = property.FindPropertyRelative("p_AssetPath").stringValue;
+            return GetObjectAtPath(path);
+        }
+        private static void ApplyProperty(SerializedProperty property, UnityEngine.Object obj)
+        {
+            if (obj == null)
+            {
+                property.FindPropertyRelative("p_AssetGUID").stringValue = string.Empty;
+                property.FindPropertyRelative("p_AssetPath").stringValue = string.Empty;
+                property.FindPropertyRelative("p_SubAssetName").stringValue = string.Empty;
+                return;
+            }
+
+            string path = AssetDatabase.GetAssetPath(obj);
+            property.FindPropertyRelative("p_AssetPath").stringValue = path;
+            property.FindPropertyRelative("p_AssetGUID").stringValue = AssetDatabase.AssetPathToGUID(path);
+
+            if (AssetDatabase.IsSubAsset(obj))
+            {
+                property.FindPropertyRelative("p_SubAssetName").stringValue = obj.name;
+            }
+            else
+            {
+                property.FindPropertyRelative("p_SubAssetName").stringValue = string.Empty;
+            }
+        }
+
+        private Type GetFieldType()
+        {
+            if (fieldInfo.FieldType.GetGenericArguments().Length == 0)
+            {
+                return TypeHelper.TypeOf<UnityEngine.Object>.Type;
+            }
+
+            return fieldInfo.FieldType.GetGenericArguments()[0];
+        }
+        
+
+        protected override void OnPropertyGUI(ref AutoRect autoRect, SerializedProperty property, GUIContent label)
+        {
+            Type targetType = GetFieldType();
+
+            Rect rect = autoRect.Pop();
+
+            using (var change = new EditorGUI.ChangeCheckScope())
+            {
+                UnityEngine.Object obj = EditorGUI.ObjectField(rect, property.displayName, LoadAsset(property), targetType, false);
+
+                if (change.changed)
+                {
+                    ApplyProperty(property, obj);
+                }
+            }
         }
     }
 }
