71ea0231cd3f8a770f0073137a885ec64657902a
Syadeu <syadeu@gmail.com>
2022-07-11 13:34:27 +0900

    a

---
+++ Editor/DragAndDropManipulator.cs
@@ -0,0 +1,153 @@
+﻿// Copyright 2022 Ikina Games
+// Author : Seung Ha Kim (Syadeu)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+#define DEBUG_MODE
+#endif
+
+#if UNITY_2019_1_OR_NEWER
+#define UNITYENGINE
+#if UNITY_2019 || !UNITY_2020_OR_NEWER
+#define UNITYENGINE_OLD
+#endif
+#else
+#define POINT_COLLECTIONS_NATIVE
+#endif
+
+#if UNITYENGINE
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Point.Collections.Editor
+{
+    // https://docs.unity3d.com/2021.2/Documentation/Manual/UIE-create-drag-and-drop-ui.html
+    public class DragAndDropManipulator : PointerManipulator
+    {
+        public const string SlotClassName = "dragNdrop-slot";
+
+        private Vector2 targetStartPosition { get; set; }
+        private Vector3 pointerStartPosition { get; set; }
+
+        private bool enabled { get; set; }
+        private VisualElement root { get; }
+
+        public DragAndDropManipulator(VisualElement root, VisualElement target)
+        {
+            this.target = target;
+            this.root = root;
+        }
+
+        protected override void RegisterCallbacksOnTarget()
+        {
+            target.RegisterCallback<PointerDownEvent>(PointerDownHandler);
+            target.RegisterCallback<PointerMoveEvent>(PointerMoveHandler);
+            target.RegisterCallback<PointerUpEvent>(PointerUpHandler);
+            target.RegisterCallback<PointerCaptureOutEvent>(PointerCaptureOutHandler);
+        }
+        protected override void UnregisterCallbacksFromTarget()
+        {
+            target.UnregisterCallback<PointerDownEvent>(PointerDownHandler);
+            target.UnregisterCallback<PointerMoveEvent>(PointerMoveHandler);
+            target.UnregisterCallback<PointerUpEvent>(PointerUpHandler);
+            target.UnregisterCallback<PointerCaptureOutEvent>(PointerCaptureOutHandler);
+        }
+
+        private void PointerDownHandler(PointerDownEvent evt)
+        {
+            targetStartPosition = target.transform.position;
+            pointerStartPosition = evt.position;
+            target.CapturePointer(evt.pointerId);
+            enabled = true;
+        }
+        private void PointerMoveHandler(PointerMoveEvent evt)
+        {
+            if (enabled && target.HasPointerCapture(evt.pointerId))
+            {
+                Vector3 pointerDelta = evt.position - pointerStartPosition;
+
+                target.transform.position = new Vector2(
+                    Mathf.Clamp(targetStartPosition.x + pointerDelta.x, 0, target.panel.visualTree.worldBound.width),
+                    Mathf.Clamp(targetStartPosition.y + pointerDelta.y, 0, target.panel.visualTree.worldBound.height));
+            }
+        }
+        private void PointerUpHandler(PointerUpEvent evt)
+        {
+            if (enabled && target.HasPointerCapture(evt.pointerId))
+            {
+                target.ReleasePointer(evt.pointerId);
+            }
+        }
+
+        private void PointerCaptureOutHandler(PointerCaptureOutEvent evt)
+        {
+            if (enabled)
+            {
+                //VisualElement slotsContainer = root.Q<VisualElement>("slots");
+                UQueryBuilder<VisualElement> allSlots =
+                    root.Query<VisualElement>(className: SlotClassName);
+
+                UQueryBuilder<VisualElement> overlappingSlots = allSlots.Where(OverlapsTarget);
+                VisualElement closestOverlappingSlot = FindClosestSlot(overlappingSlots);
+                Vector3 closestPos = Vector3.zero;
+                if (closestOverlappingSlot != null)
+                {
+                    closestPos = RootSpaceOfSlot(closestOverlappingSlot);
+                    closestPos = new Vector2(closestPos.x - 5, closestPos.y - 5);
+                }
+                target.transform.position =
+                    closestOverlappingSlot != null ?
+                    closestPos :
+                    targetStartPosition;
+
+                enabled = false;
+            }
+        }
+
+        private bool OverlapsTarget(VisualElement slot)
+        {
+            return target.worldBound.Overlaps(slot.worldBound);
+        }
+
+        private VisualElement FindClosestSlot(UQueryBuilder<VisualElement> slots)
+        {
+            List<VisualElement> slotsList = slots.ToList();
+            float bestDistanceSq = float.MaxValue;
+            VisualElement closest = null;
+            foreach (VisualElement slot in slotsList)
+            {
+                Vector3 displacement =
+                    RootSpaceOfSlot(slot) - target.transform.position;
+                float distanceSq = displacement.sqrMagnitude;
+                if (distanceSq < bestDistanceSq)
+                {
+                    bestDistanceSq = distanceSq;
+                    closest = slot;
+                }
+            }
+            return closest;
+        }
+
+        private Vector3 RootSpaceOfSlot(VisualElement slot)
+        {
+            Vector2 slotWorldSpace = slot.parent.LocalToWorld(slot.layout.position);
+            return root.WorldToLocal(slotWorldSpace);
+        }
+    }
+}
+
+#endif
