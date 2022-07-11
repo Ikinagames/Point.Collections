176f9551bd82479a7979ced0d4e6f40c6e68c738
Syadeu <syadeu@gmail.com>
2022-07-11 10:56:53 +0900

    a

--- Runtime/Point.Collections.SceneManagement/CommonShader.hlsl
+++ Runtime/Point.Collections.SceneManagement/CommonShader.hlsl
@@ -1,18 +1,17 @@
 #ifndef COMMONSHADER_INCLUDED
 #define COMMONSHADER_INCLUDED
 
+// https://docs.unity3d.com/Packages/com.unity.shadergraph@6.7/manual/Custom-Function-Node.html
 // https://stackoverflow.com/questions/12030101/an-outline-sharp-transition-in-a-fragment-shader
-float outline(float t, float threshold, float width){
-    float result = clamp(width - abs(threshold - t) / fwidth(t), 0.0, 1.0);
-
-    return result;
+void outline(float t, float threshold, float width, out float result){
+    result = clamp(width - abs(threshold - t) / fwidth(t), 0.0, 1.0);
 }
-float outlineEuclidean(float t, float threshold, float width)
-{
-    float dx = dFdx(t);
-    float dy = dFdy(t);
-    float ewidth = sqrt(dx * dx + dy * dy);
-    return clamp(width - abs(threshold - t) / ewidth, 0.0, 1.0);
-}
+//float outlineEuclidean(float t, float threshold, float width)
+//{
+//    float dx = dFdx(t);
+//    float dy = dFdy(t);
+//    float ewidth = sqrt(dx * dx + dy * dy);
+//    return clamp(width - abs(threshold - t) / ewidth, 0.0, 1.0);
+//}
 
 #endif // COMMONSHADER_INCLUDED
