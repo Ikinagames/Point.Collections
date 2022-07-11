5fda3e6cdba3b821be394cbab559a126349b795b
Syadeu <syadeu@gmail.com>
2022-07-11 17:15:23 +0900

    a

---
+++ Editor/RulerView.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2022 Ikina Games
+// Author : Seung Ha Kim (Syadeu)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if UNITY_2019_1_OR_NEWER
+#if (UNITY_EDITOR || DEVELOPMENT_BUILD) && !POINT_DISABLE_CHECKS
+#define DEBUG_MODE
+#endif
+#define UNITYENGINE
+
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace Point.Collections.Editor
+{
+    public sealed class RulerView : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<RulerView, UxmlTraits> { }
+        public new class UxmlTraits : VisualElement.UxmlTraits
+        {
+            public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
+            {
+                get { yield break; }
+            }
+            public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+            {
+                base.Init(ve, bag, cc);
+                RulerView ate = ve as RulerView;
+            }
+        }
+
+        private VisualElement m_ContentContainer;
+
+        public override VisualElement contentContainer => m_ContentContainer;
+
+        public RulerView()
+        {
+            m_ContentContainer = new VisualElement();
+        }
+    }
+}
+
+#endif
